8da126bb691a0f4715046439439fbf91afd3c47c
Frank <frankz.lu@gmail.com>
2019-06-14 12:23:54 +0200

    test

---
+++ my_test.py
@@ -0,0 +1,128 @@
+import tensorflow as tf
+import tools.mnist as mnist_tools
+import tools.semisup as semisup
+import numpy as np
+import architectures as arch
+
+import tools.visualization as vis
+import tools.updated_semisup as up
+
+train_images, train_labels = mnist_tools.get_data('train')
+test_images, test_labels = mnist_tools.get_data('train')
+
+# labeled samples used per class
+sup_per_class = 10
+sup_seed = -1
+# labeled samples per class per batch
+sup_per_batch = 10
+unsup_batch_size = 100
+
+
+learning_rate = 1e-3
+decay_steps = 5000
+decay_factor = 0.33
+visit_weight = 0.0
+
+max_steps = 10000
+eval_interval = 500
+
+log_dir = "logs/model"
+
+seed = None
+
+NUM_LABELS = mnist_tools.NUM_LABELS
+IMAGE_SHAPE = mnist_tools.IMAGE_SHAPE
+
+# gets sup_per_class samples for each class
+
+# [10 (classes), 10 (samples), 28, 28, 1]
+
+sup_by_label = semisup.sample_by_label(train_images, train_labels,
+                                sup_per_class, NUM_LABELS, seed)
+
+from ipywidgets import IntProgress, Layout
+
+graph = tf.Graph()
+
+
+with graph.as_default():
+    
+    model = semisup.SemisupModel(arch.mnist_model, NUM_LABELS, IMAGE_SHAPE)
+    
+    # iterator = up.create_input(train_images, train_labels, unsup_batch_size)
+    # t_unsup_images, _ = up.create_input(train_images, train_labels, unsup_batch_size)
+    
+    #it = up.create_input(train_images, train_labels, unsup_batch_size)
+    #t_unsup_images,_ = it.get_next()
+    
+    unsup_data = up.create_input(train_images, train_labels, unsup_batch_size)
+    unsup_it   = unsup_data.make_initializable_iterator()
+    
+    t_unsup_images,_ = unsup_it.get_next()
+    
+    
+    sup_data = up.create_per_class_inputs(sup_by_label, sup_per_batch)
+    sup_it   = sup_data.make_one_shot_iterator()
+    
+    item       = sup_it.get_next()
+    temp_unzip = (list(zip(*item)))
+    t_sup_images, t_sup_labels = tf.concat(temp_unzip[0], 0), tf.concat(temp_unzip[1], 0)
+    
+    #t_sup_images, t_sup_labels = up.create_per_class_inputs(sup_by_label, sup_per_batch)
+    
+    t_sup_emb   = model.image_to_embedding(t_sup_images)
+    t_unsup_emb = model.image_to_embedding(t_unsup_images)
+    
+    t_sup_logit = model.embedding_to_logit(t_sup_emb)
+    
+    model.add_semisup_loss(t_sup_emb, t_unsup_emb, t_sup_labels, visit_weight=visit_weight)
+    model.add_logit_loss(t_sup_logit, t_sup_labels)
+    
+    t_learning_rate = tf.train.exponential_decay(
+        learning_rate,
+        model.step,
+        decay_steps,
+        decay_factor,
+        staircase = True
+    )
+    
+    train_op = model.create_train_op(t_learning_rate)
+    summary_op = tf.summary.merge_all()
+    
+    summary_writer = tf.summary.FileWriter(log_dir, graph)
+    
+    saver = tf.train.Saver()
+
+
+with tf.Session(graph=graph) as sess:
+    sess.run(unsup_it.initializer)
+    sess.run(tf.global_variables_initializer())
+    
+    epoch = 1
+    
+    for step in range(max_steps):
+        try:
+            _, summaries = sess.run([train_op, summary_op])
+        except tf.errors.OutOfRangeError:
+            sess.run(unsup_it.initializer)
+            epoch += 1
+            _, summaries = sess.run([train_op, summary_op])
+            
+        if (step + 1) % eval_interval == 0 or step == 99:
+            print('Step: %d' % step)
+            test_pred = model.classify(test_images).argmax(-1)
+            conf_mtx = semisup.confusion_matrix(test_labels, test_pred, NUM_LABELS)
+            test_err = (test_labels != test_pred).mean() * 100
+            print(conf_mtx)
+            print('Test error: %.2f %%' % test_err)
+            print()
+    
+            test_summary = tf.Summary(
+                value=[tf.Summary.Value(
+                    tag='Test Err', simple_value=test_err)])
+    
+            summary_writer.add_summary(summaries, step)
+            summary_writer.add_summary(test_summary, step)
+
+            print(f"saving model - step {model.step}")
+            saver.save(sess, log_dir, global_step=model.step)
